2ec6abc93ed360a1006ebce68a2361dee689573b
ndkhuong24 <kn134646@gmail.com>
2026-08-12 23:10:26 +0700

    Bài tập bài 10

---
+++ src/Bai10.java
@@ -0,0 +1,20 @@
+public class Bai10 {
+    public static void main(String[] args) {
+        String name = "Nguyễn Duy Khương";
+
+        // In toàn bộ name
+        System.out.println(name);
+
+        // In độ dài của name
+        System.out.println(name.length());
+
+        // In ký tự đầu tiên
+        System.out.println(name.charAt(0));
+
+        // In name viết hoa
+        System.out.println(name.toUpperCase());
+
+        // In name viết thường
+        System.out.println(name.toLowerCase());
+    }
+}
---
+++ src/BaiTapPhan10.java
@@ -0,0 +1,9 @@
+public class BaiTapPhan10 {
+    public static void main(String[] args) {
+        String text = "Hello Java";
+
+        // a, e, i, o, u
+
+
+    }
+}
