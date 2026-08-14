c6696dc362f94e823896664b53e403f40cf2b8c2
ndkhuong24 <kh134646@gmail.com>
2026-08-14 16:23:29 +0700

    b11

--- src/Bai11.java
+++ src/Bai11.java
@@ -21,26 +21,42 @@ public class Bai11 {
 //    }
 
     // Bài tập bài 11
-    public static int add(int a, int b) {
-        return a + b;
-    }
+//    public static int add(int a, int b) {
+//        return a + b;
+//    }
+//
+//    public static int multiply(int a, int b) {
+//        return a * b;
+//    }
+//
+//    public static boolean isEven(int number) {
+    /// /        return number % 2 == 0; kiểu này ngắn gọn hơn
+//        if (number % 2 == 0) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(add(1, 2));
+//        System.out.println(multiply(1, 2));
+//        System.out.println(isEven(1));
+//    }
 
-    public static int multiply(int a, int b) {
-        return a * b;
-    }
+    static  int[] numbers = {10, 25, 7, 42, 18, 3, 31};
 
-    public static boolean isEven(int number) {
-//        return number % 2 == 0; kiểu này ngắn gọn hơn
-        if (number % 2 == 0) {
-            return true;
-        } else {
-            return false;
+    public static int findMax(int[] numbersArray) {
+        int max = numbersArray[0];
+        for (int i = 0; i < numbersArray.length; i++) {
+            if (numbersArray[i] > max) {
+                max = numbersArray[i];
+            }
         }
+        return max;
     }
 
     public static void main(String[] args) {
-        System.out.println(add(1, 2));
-        System.out.println(multiply(1, 2));
-        System.out.println(isEven(1));
+        System.out.println(findMax(numbers));
     }
 }
