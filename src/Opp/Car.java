d43b02491e6f4278932aaa1e6a7586933f52af46
ndkhuong24 <kh134646@gmail.com>
2026-08-19 20:16:51 +0700

    oop

--- src/Opp/Car.java
+++ src/Opp/Car.java
@@ -6,11 +6,16 @@ public class Car {
     String color;
     int speed;
 
+    Car (String brand, String color, int speed) {
+        this.brand = brand;
+        this.color = color;
+        this.speed = speed;
+    }
+
     // METHOD — hành động
     void driver() {
         System.out.println("Driver");
     }
-
     String action(String command) {
         return command;
     }
