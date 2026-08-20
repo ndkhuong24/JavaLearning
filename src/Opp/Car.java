3e792862190feb9ab2ded0f95c446524a4ca407c
ndkhuong24 <kh134646@gmail.com>
2026-08-20 15:10:05 +0700

    Class OOP

--- src/Opp/Car.java
+++ src/Opp/Car.java
@@ -2,21 +2,47 @@ package Opp;
 
 public class Car {
     // FIELD — dữ liệu
-    String brand;
-    String color;
-    int speed;
+    private String brand;
+    private String color;
+    private int speed;
 
-    Car (String brand, String color, int speed) {
+    Car(String brand, String color, int speed) {
         this.brand = brand;
         this.color = color;
-        this.speed = speed;
+        setSpeed(speed);
     }
 
-    // METHOD — hành động
-    void driver() {
-        System.out.println("Driver");
+    public int getSpeed() {
+        return speed;
     }
-    String action(String command) {
-        return command;
+
+    public String getColor() {
+        return color;
     }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setSpeed(int speed) {
+        if (speed >= 0) {
+            this.speed = speed;
+        }
+    }
+
+    //    // METHOD — hành động
+//    void driver() {
+//        System.out.println("Driver");
+//    }
+//    String action(String command) {
+//        return command;
+//    }
 }
--- src/Opp/Main.java
+++ src/Opp/Main.java
@@ -2,13 +2,20 @@ package Opp;
 
 public class Main {
     public static void main(String[] args) {
-        Car car1 = new Car(); // Đây là 1 object
-        car1.brand = "BMW"; //
-        car1.color = "Red"; //Field
-        car1.speed = 120;   //
-        car1.driver(); // method
-        System.out.println(car1.action("Bạn đang lái xe"));//Chỉ trả về dữ liệu phải dùng sout để in ra
-        System.out.println(car1.brand + " " + car1.color + " " + car1.speed);
+//        Car car1 = new Car(); // Đây là 1 object
+//        car1.brand = "BMW"; //
+//        car1.color = "Red"; //Field
+//        car1.speed = 120;   //
+
+        Car car1 = new Car("BMW", "Red", 120);
+        Car car2 = new Car("Toyota", "Black", -100);
+
+        System.out.println(car2.getSpeed());
+
+//        car1.driver(); // method
+//        System.out.println(car1.action("Bạn đang lái xe"));//Chỉ trả về dữ liệu phải dùng sout để in ra
+//        System.out.println(car1.brand + " " + car1.color + " " + car1.speed);
+
 
     }
 }
