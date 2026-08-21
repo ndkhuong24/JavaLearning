44d7346f7c14061d76d7dc56f5487299317372f6
ndkhuong24 <kn134646@gmail.com>
2026-08-21 22:51:38 +0700

    OOP

--- src/OopTutorial/BankAccount.java
+++ src/OopTutorial/BankAccount.java
@@ -7,18 +7,18 @@ public class BankAccount {
         this.balance = balance;
     }
 
-//    public void deposit(double money) {
-//        if (money > 0) {
-//            balance += money;
-//        }
-//    }
-//
-//    public double withdraw(double money) {
-//        if (money > 0 && money <= balance) {
-//            balance -= money;
-//        }
-//        return balance;
-//    }
+    public void deposit(double money) {
+        if (money > 0) {
+            balance += money;
+        }
+    }
+
+    public double withdraw(double money) {
+        if (money > 0 && money <= balance) {
+            balance -= money;
+        }
+        return balance;
+    }
 
     public double getBalance() {
         return balance;
--- src/OopTutorial/Main.java
+++ src/OopTutorial/Main.java
@@ -2,12 +2,9 @@ package OopTutorial;
 
 public class Main {
     public static void main(String[] args) {
-        BankAccount account = new BankAccount(1000);
-        System.out.println(account.getBalance());
-
-//        account.deposit(1000);
-//        account.withdraw(500);
-//
-//        System.out.println(account.getBalance());
+        BankAccount bankAccount = new BankAccount(0);
+        bankAccount.deposit(1000);
+        bankAccount.withdraw(500);
+        System.out.println(bankAccount.getBalance());
     }
 }
