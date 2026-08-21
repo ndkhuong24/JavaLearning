5b5a02b96aaa150043746da7985547aeaa231dca
ndkhuong24 <kn134646@gmail.com>
2026-08-21 23:41:12 +0700

    OOP

---
+++ src/Inheritance/Vehicle.java
@@ -0,0 +1,11 @@
+package Inheritance;
+
+public class Vehicle {
+    protected String brand;
+    protected int speed;
+
+    public Vehicle(String brand, int speed) {
+        this.brand = brand;
+        this.speed = speed;
+    }
+}
